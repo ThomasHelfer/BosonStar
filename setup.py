9e30ebd391b3a9d865ae1e65410888d630d9bedd
Thomas <thomashelfer@live.de>
2020-02-10 14:40:26 -0500

    Added setup file

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from setuptools import find_packages
+
+def readme():
+	with open('README.rst') as f:
+		return f.read()
+
+setup(name='bosonstar',
+	version='0.1',
+	description='HBA with GPR and PCA',
+	long_description=readme(),
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 2.7',
+		'Topic :: Gravitational Wave :: Physics',
+	],
+	keywords='Boson Stars',
+#	url='http://github.com/storborg/funniest',
+	author='ThomasHelfer',
+	author_email='thomashelfer@live.de',
+	license='MIT',
+	packages=find_packages(),
+	install_requires=[
+		'numpy',
+		'scipy',
+	],
+	include_package_data=True,
+	zip_safe=False)
